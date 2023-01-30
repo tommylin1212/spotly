82f9e49415031195a36092fcb52416035bee9d3e
Caleb Tomlin <caleb.w.tomlin@gmail.com>
2023-01-30 11:09:52 -0600

    initial commit

---
+++ pages/api/ptest.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+    if (req.method === 'POST') {
+        res.status(200).json({name:'post accepted'})
+      } else {
+        res.status(400).json({ name: 'post to this endpoint' })
+      }
+    }
+
